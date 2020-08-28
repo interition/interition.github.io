e7af61a8e245bcd99f11c536cb8ee396883e60b0
Paul Worrall <paul.worrall@interition.net>
2020-08-28 13:01:27 +0100

    Updates

---
+++ precache-manifest.e89dd6152678113ec5cfa9a1eeefc313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c227b1bb3d986f742337861e03075af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b1b99ab2549d8cc012",
+    "url": "/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "68ecbc0271e6c2bb7b27",
+    "url": "/static/js/2.472c030a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.472c030a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b1b99ab2549d8cc012",
+    "url": "/static/js/main.333f276c.chunk.js"
+  },
+  {
+    "revision": "7dfb825ea48717172566",
+    "url": "/static/js/runtime-main.d7b1c651.js"
+  }
+]);
